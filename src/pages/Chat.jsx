c7d9250abb2a949328c8d8ec6ce6b6543019a84a
mac <mac@macui-MacBookAir.local>
2023-07-11 16:39:38 +0900

    src

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,291 @@
+import { useEffect, useRef, useState } from "react";
+import { io, Manager } from "socket.io-client";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faVideo,
+  faMicrophone,
+  faMessage,
+} from "@fortawesome/free-solid-svg-icons";
+
+import user_img from "../assets/user.svg";
+
+const maxWidth = "620px";
+const Chat = () => {
+  const socketRef = useRef();
+  const pcRef = useRef();
+  const localVideoRef = useRef(null);
+  const remoteVideoRef = useRef(null);
+  const [states, setStates] = useState({
+    mic: false,
+    video: false,
+  });
+  const [remote_img, setRemote_Img] = useState(false);
+  const streamRef = useRef(null);
+  const setVideoTracks = async () => {
+    try {
+      streamRef.current = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      });
+      if (localVideoRef.current)
+        localVideoRef.current.srcObject = streamRef.current;
+      if (!(pcRef.current && socketRef.current)) return;
+      streamRef.current.getTracks().forEach((track) => {
+        if (!pcRef.current) return;
+        pcRef.current.addTrack(track, streamRef.current);
+        setStates((prev) => {
+          return {
+            video: true,
+            mic: true,
+          };
+        });
+      });
+      pcRef.current.onicecandidate = (e) => {
+        if (e.candidate) {
+          if (!socketRef.current) return;
+          socketRef.current.emit("candidate", e.candidate);
+        }
+      };
+      pcRef.current.ontrack = (ev) => {
+        if (remoteVideoRef.current) {
+          remoteVideoRef.current.srcObject = ev.streams[0];
+        }
+      };
+      socketRef.current.emit("join_room", {
+        room: "1234",
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const createOffer = async () => {
+    console.log("create offer", pcRef.current);
+    if (!(pcRef.current && socketRef.current)) return;
+    try {
+      const offerOptions = {
+        offerToReceiveAudio: true,
+        offerToReceiveVideo: true,
+      };
+
+      const sdp = await pcRef.current.createOffer(offerOptions);
+      await pcRef.current.setLocalDescription(sdp);
+
+      socketRef.current.emit("offer", sdp);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  const createAnswer = async (sdp) => {
+    if (!(pcRef.current && socketRef.current)) return;
+    try {
+      await pcRef.current.setRemoteDescription(sdp);
+      const mySdp = await pcRef.current.createAnswer({
+        offerToReceiveVideo: true,
+        offerToReceiveAudio: true,
+      });
+      await pcRef.current.setLocalDescription(mySdp);
+      socketRef.current.emit("answer", mySdp);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    socketRef.current = io("localhost:8080");
+
+    pcRef.current = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun4.l.google.com:19302",
+        },
+      ],
+    });
+
+    socketRef.current.on("all_users", (allUsers) => {
+      if (allUsers.length > 0) {
+        createOffer();
+      }
+    });
+
+    socketRef.current.on("getOffer", (sdp) => {
+      createAnswer(sdp);
+    });
+
+    socketRef.current.on("getAnswer", (sdp) => {
+      if (!pcRef.current) return;
+      pcRef.current.setRemoteDescription(sdp);
+    });
+
+    socketRef.current.on("getCandidate", async (candidate) => {
+      if (!pcRef.current) return;
+      await pcRef.current.addIceCandidate(candidate);
+      console.log("candidate add success");
+    });
+    socketRef.current.on("return_video_state", (state) => {
+      console.log(state);
+    });
+
+    setVideoTracks();
+    socketRef.current.on("user_exit", (id) => {});
+    const senders = pcRef.current.getSenders();
+    console.log(senders);
+    return () => {
+      socketRef.current.disconnect();
+      pcRef.current.close();
+    };
+  }, []);
+  const handleCamera = async () => {
+    streamRef.current
+      .getVideoTracks()
+      .forEach((track) => (track.enabled = !track.enabled));
+    socketRef.current.emit("video_state", states.video);
+
+    setStates((prev) => {
+      return {
+        ...prev,
+        video: !prev.video,
+      };
+    });
+  };
+  const handleMic = async () => {
+    streamRef.current
+      .getAudioTracks()
+      .forEach((track) => (track.enabled = !track.enabled));
+    setStates((prev) => {
+      return {
+        ...prev,
+        mic: !prev.mic,
+      };
+    });
+  };
+  return (
+    <Container>
+      <Main className="main">
+        <VideoWrapper>
+          <Video autoPlay ref={localVideoRef} />
+          <DisConnectImg src={user_img} />
+        </VideoWrapper>
+        <VideoWrapper>
+          <Video autoPlay ref={remoteVideoRef} />
+          <DisConnectImg src={user_img} />
+        </VideoWrapper>
+      </Main>
+      <Nav>
+        <CameraBtn videostate={states.video.toString()} onClick={handleCamera}>
+          <FontAwesomeIcon icon={faVideo} />
+        </CameraBtn>
+        <MicBtn micstate={states.mic.toString()} onClick={handleMic}>
+          <FontAwesomeIcon icon={faMicrophone} />
+        </MicBtn>
+        <MessageBtn>
+          <FontAwesomeIcon icon={faMessage} />
+        </MessageBtn>
+      </Nav>
+    </Container>
+  );
+};
+
+export default Chat;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: #202124;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  @media screen and (max-width: ${maxWidth}) {
+    align-items: flex-start;
+  }
+`;
+
+const Main = styled.main`
+  display: flex;
+  width: 90%;
+  height: 80%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 80px;
+  @media screen and (max-width: ${maxWidth}) {
+    margin-top: 40px;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    padding: 0px;
+  }
+`;
+
+const Video = styled.video`
+  height: 400px;
+  z-index: 88;
+  border-radius: 15px;
+  @media screen and (max-width: ${maxWidth}) {
+    height: 270px;
+  }
+`;
+
+const CameraBtn = styled.div`
+  background-color: ${(props) =>
+    props.videostate === "true" ? "red" : "green"};
+`;
+const Nav = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  > div {
+    width: 40px;
+    height: 40px;
+    border-radius: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 4;
+    color: white;
+    margin-right: 15px;
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+  }
+`;
+
+const MicBtn = styled.div`
+  background-color: ${(props) => (props.micstate === "true" ? "red" : "green")};
+`;
+const MessageBtn = styled.div`
+  background-color: #3d4043;
+`;
+const VideoWrapper = styled.div`
+  border-radius: 15px;
+  position: relative;
+  z-index: 3;
+  background-color: #3d4043;
+  height: 400px;
+  width: 530px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: ${maxWidth}) {
+    height: 269px;
+    width: 358px;
+    margin-bottom: 30px;
+  }
+`;
+
+const DisConnectImg = styled.img`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const Name = styled.span`
+  position: absolute;
+  z-index: 99;
+  bottom: 5%;
+  left: 5%;
+  color: white;
+`;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,110 @@
+import gsap from "gsap";
+import { useEffect } from "react";
+import { styled } from "styled-components";
+import z from "../assets/z.svg";
+import o from "../assets/o.svg";
+import oo from "../assets/oo.svg";
+import ooo from "../assets/ooo.svg";
+import m from "../assets/m.svg";
+import { Link } from "react-router-dom";
+const Home = () => {
+  useEffect(() => {
+    gsap.from(".z", { x: -100, opacity: 0, rotateZ: 100, duration: 2 });
+    gsap.from(".o_1", {
+      y: -200,
+      opacity: 0,
+      rotateX: 200,
+      duration: 2,
+      delay: 1,
+    });
+    gsap.from(".o_2", {
+      y: 200,
+      x: 100,
+      opacity: 0,
+      rotateX: 200,
+      duration: 2,
+      delay: 0,
+    });
+    gsap.from(".o_3", {
+      y: -200,
+      x: 100,
+      opacity: 0,
+      rotateX: 200,
+      duration: 2,
+      delay: 4,
+    });
+    gsap.from(".m", {
+      x: 100,
+      opacity: 0,
+      rotateX: 200,
+      duration: 2,
+      delay: 1,
+    });
+  }, []);
+  return (
+    <Container>
+      <Main>
+        <Content>
+          <img src={z} width="80" height="80" className="z" />
+          <img src={o} width="80" height="80" className="o_1" />
+          <img src={oo} width="80" height="80" className="o_2" />
+          <img src={ooo} width="80" height="80" className="o_3" />
+          <img src={m} width="80" height="80" className="m" />
+        </Content>
+        <Span>방문 해주셔서 감사합니다.</Span>
+        <A to="/chat">화상채팅 해보기</A>
+      </Main>
+    </Container>
+  );
+};
+export default Home;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  --tw-bg-opacity: 1;
+  background-color: rgb(254 242 242 / var(--tw-bg-opacity));
+`;
+
+const Main = styled.main`
+  width: 75%;
+  height: 66.666667%;
+  display: flex;
+  flex-direction: column;
+  padding: 0.75rem;
+  padding-top: 2.75rem;
+`;
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 6rem;
+`;
+
+const Span = styled.span`
+  --tw-text-opacity: 1;
+  color: rgb(216 180 254 / var(--tw-text-opacity));
+  text-align: center;
+  font-size: 1.875rem /* 30px */;
+  line-height: 2.25rem /* 36px */;
+`;
+
+const A = styled(Link)`
+  --tw-text-opacity: 1;
+  color: rgb(216 180 254 / var(--tw-text-opacity));
+  width: fit-content;
+  margin: auto;
+  text-align: center;
+  font-size: 1.5rem /* 24px */;
+  line-height: 2rem /* 32px */;
+  margin-top: 4rem;
+  transition: 0.3s ease-in-out;
+  &:hover {
+    --tw-text-opacity: 1;
+    color: rgb(107 114 128 / var(--tw-text-opacity));
+  }
+`;
